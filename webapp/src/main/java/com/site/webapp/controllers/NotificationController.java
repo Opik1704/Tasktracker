23965660e46ae7dd9bcba6bf4639e170a05f90ff
Opik1704 <dsgorch04@gmail.com>
2026-03-28 00:21:43 +0300

    КОлокольчик выпадает вниз

---
+++ webapp/src/main/java/com/site/webapp/controllers/NotificationController.java
@@ -0,0 +1,31 @@
+package com.site.webapp.controllers;
+
+import com.site.webapp.models.User;
+import com.site.webapp.service.NotificationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/notifications")
+public class NotificationController extends LoggingController {
+
+    @Autowired
+    private NotificationService notificationService;
+
+    @PostMapping("/notifications/mark-read")
+    public String markAllAsRead(@RequestHeader(value = "referer", required = false) String referer) {
+        User user = getCurrentUser();
+        if (user != null) {
+            notificationService.markAllAsRead(user);
+        }
+
+        if (referer != null && !referer.isEmpty()) {
+            return "redirect:" + referer;
+        }
+        return "redirect:/all-tasks";
+    }
+}
